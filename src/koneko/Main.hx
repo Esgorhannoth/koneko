b2318b8af8bbd30be96a3cb187513b1651b57375
Esgorhannoth <esgorhannoth@gmail.com>
2017-01-18 14:27:27 +0400

    interpret file from command line

--- src/koneko/Main.hx
+++ src/koneko/Main.hx
@@ -8,7 +8,26 @@ class Main {
       repl();
     else {
       var i = new Interpreter();
-      i.interpret(args[0]);
+      try {
+        if( (args[0] == "e" || args[0] == "-e") && (args.length >= 2) ) {
+          i.interpret(args[1]);
+        }
+        // not 'e' or '-e'
+        else {
+          var fn = args[0];
+          if( sys.FileSystem.exists(fn) ) {
+            var body = sys.io.File.getContent(fn);
+            i.interpret(body);
+          }
+          else
+            throw 'File $fn not found.';
+        }
+
+      }
+      catch(e: Dynamic) {
+        Sys.print("Error: ");
+        Sys.println(e);
+      }
     }
   }
 
