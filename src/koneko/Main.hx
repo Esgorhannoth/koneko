fbc2e7e2592424feb7f26e348a5c340554841a2e
Esgorhannoth <esgorhannoth@gmail.com>
2017-01-19 16:53:47 +0400

    BUG: some cli flags implemented

--- src/koneko/Main.hx
+++ src/koneko/Main.hx
@@ -1,32 +1,99 @@
 package koneko;
 
+
+enum Action {
+  Eval(s: String);
+  EvalClean(s: String);
+  LoadFile(f: String);
+}
+
 class Main {
+  static var prelude = true;
+
   public static function main() {
     var args = Sys.args();
     if( args.length <= 0 )
-      // throw "REPL is not yet implemented.";
       repl();
-    else {
-      var i = new Interpreter();
-      try {
-        if( (args[0] == "e" || args[0] == "-e") && (args.length >= 2) ) {
-          i.interpret(args[1]);
-        }
-        // not 'e' or '-e'
-        else {
-          var fn = args[0];
-          if( sys.FileSystem.exists(fn) ) {
-            var body = sys.io.File.getContent(fn);
-            i.interpret(body);
-          }
-          else
-            throw 'File $fn not found.';
-        }
 
+    try {
+      var actions = parse(args);
+      run(actions);
+    }
+    catch(e: Dynamic) handleErrors(e);
+  }
+
+  static function parse(args: Array<String>): Array<Action> {
+    var actions = new Array<Action>();
+    while(true) {
+      // parse args
+      var actions = new Array<Action>();
+      var arg = args.shift();
+      switch( arg ) {
+        case "-f" | "--load":
+          var fn = args.shift();
+          if( fn == null )
+            throw "No filename for -f|--load";
+          actions.push(LoadFile(fn));
+        case "-e" | "--eval":
+          var body = args.shift();
+          if( body == null )
+            throw "No program for -e|--eval";
+          actions.push(Eval(body));
+        case "-E" | "--Eval":
+          var body = args.shift();
+          if( body == null )
+            throw "No program for -E|--Eval";
+          actions.push(EvalClean(body));
+        case "-n" | "--no-prelude":
+          prelude = false;
+        case null: if( actions.length <= 0 ) repl();
+        case _   :
+                   if( StringTools.startsWith(arg, "-") )
+                     throw 'Unknown option: $arg';
+                   else
+                     actions.push(LoadFile(arg));
       }
-      catch(e: Dynamic) {
-        Sys.print("Error: ");
-        Sys.println(e);
+    } // while
+    return actions;
+  }
+
+  static function interp(s: String, ?intp: Interpreter) {
+    var i: Interpreter;
+    if( null == intp ) {
+      i = new Interpreter();
+      i = load_prelude(i);
+    } else
+      i = intp;
+    i.interpret(s);
+  }
+
+  static function load_file(fn: String, i: Interpreter): Interpreter {
+    if( sys.FileSystem.exists(fn) ) {
+      var body = sys.io.File.getContent(fn);
+      i.interpret(body);
+    }
+    return i;
+  }
+
+  static function load_prelude(i: Interpreter): Interpreter {
+    if( prelude )
+      i = load_file("Prelude.kn", i);
+    return i;
+  }
+
+  static function handleErrors(e: Dynamic) {
+    Sys.print("Error: ");
+    Sys.println(e);
+  }
+
+  static function run(actions: Array<Action>) {
+    var i = new Interpreter();
+    i = load_prelude(i);
+    for ( a in actions ) {
+      switch( a ) {
+        case Eval(s) : interp(s, i);
+        case EvalClean(s) : interp(s);
+        case LoadFile(f):
       }
     }
   }
@@ -34,6 +101,7 @@ class Main {
   static function repl() {
     var line = "";
     var i = new Interpreter();
+    i = load_prelude(i);
     while(true) {
       Sys.stdout().writeString("> ");
       Sys.stdout().flush();
