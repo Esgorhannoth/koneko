b42c6844101feca2160e5420b35fb7ca412871be
Esgorhannoth <esgorhannoth@gmail.com>
2017-01-19 21:17:56 +0400

    Prelude.kn added; -f, -e, -E, -i, -n flags

--- src/koneko/Main.hx
+++ src/koneko/Main.hx
@@ -5,13 +5,17 @@ enum Action {
   Eval(s: String);
   EvalClean(s: String);
   LoadFile(f: String);
+  Repl;
 }
 
 class Main {
-  static var prelude = true;
+  static var prelude    = true;
+  static var start_repl = false;
 
   public static function main() {
     var args = Sys.args();
+
+    // no args - repl anyway
     if( args.length <= 0 )
       repl();
 
@@ -26,27 +30,34 @@ class Main {
     var actions = new Array<Action>();
     while(true) {
       // parse args
-      var actions = new Array<Action>();
       var arg = args.shift();
       switch( arg ) {
+        case "-i" | "--repl": start_repl = true;
         case "-f" | "--load":
           var fn = args.shift();
           if( fn == null )
             throw "No filename for -f|--load";
           actions.push(LoadFile(fn));
+
         case "-e" | "--eval":
           var body = args.shift();
           if( body == null )
             throw "No program for -e|--eval";
           actions.push(Eval(body));
+
         case "-E" | "--Eval":
           var body = args.shift();
           if( body == null )
             throw "No program for -E|--Eval";
           actions.push(EvalClean(body));
+
         case "-n" | "--no-prelude":
           prelude = false;
-        case null: if( actions.length <= 0 ) repl();
+
+        case null:
+          if( actions.length <= 0 ) actions.push(Repl);
+          break;
+
         case _   :
                    if( StringTools.startsWith(arg, "-") )
                      throw 'Unknown option: $arg';
@@ -54,6 +65,8 @@ class Main {
                      actions.push(LoadFile(arg));
       }
     } // while
+
+    if( start_repl ) actions.push(Repl);
     return actions;
   }
 
@@ -93,15 +106,20 @@ class Main {
       switch( a ) {
         case Eval(s) : interp(s, i);
         case EvalClean(s) : interp(s);
-        case LoadFile(f):
+        case LoadFile(f): i = load_file(f, i);
+        case Repl: repl(i);
       }
     }
   }
 
-  static function repl() {
+  static function repl(?intp: Interpreter) {
     var line = "";
-    var i = new Interpreter();
-    i = load_prelude(i);
+    var i: Interpreter;
+    if( null == intp ) {
+      i = new Interpreter();
+      i = load_prelude(i);
+    } else
+      i = intp;
     while(true) {
       Sys.stdout().writeString("> ");
       Sys.stdout().flush();
